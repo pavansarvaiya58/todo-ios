9a4d2202ea319d447481ec9f3e5b5459715007c1
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2023-09-25 18:34:48 -0600

    firebase setup

--- TodoList/TodoListApp.swift
+++ TodoList/TodoListApp.swift
@@ -5,10 +5,15 @@
 //  Created by Pavan Sarvaiya on 25/09/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct TodoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
