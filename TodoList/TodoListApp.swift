1acf0d6494ea3bee5473087e8816c9f8622d49af
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2023-09-25 18:02:26 -0600

    initial code

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Pavan Sarvaiya on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
