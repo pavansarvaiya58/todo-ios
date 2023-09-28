a9ba124770ffece84f292a6d4f788cc99f040e2a
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2023-09-27 19:55:26 -0600

    working on register view model

--- TodoList/ViewModels/RegisterViewViewModal.swift
+++ TodoList/ViewModels/RegisterViewViewModal.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class RegisterViewViewModal: ObservableObject {
+    
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    
+    init() {}
+    
+    func register() {
+        // Register User
+    }
+    
+    private func validate() -> Bool {
+        errorMessage = ""
+        
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty, !email.trimmingCharacters(in: .whitespaces).isEmpty, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            
+            errorMessage = "Please fill in all fields."
+
+            return false
+        }
+        
+        // email@foo.com
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter valid email."
+            return false
+        }
+        
+        return true
+    }
+    
+}
