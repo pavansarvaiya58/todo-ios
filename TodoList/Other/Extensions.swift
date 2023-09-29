8a6ef7fa495124692283639df154a2d079d505a4
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2023-09-28 19:19:13 -0600

    working on adding data to the firestore

---
+++ TodoList/Other/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  TodoList
+//
+//  Created by Pavan Sarvaiya on 28/09/23.
+//
+
+import Foundation
+
+extension Encodable {
+    func asDictionary() -> [String: Any] {
+        
+    }
+}
--- TodoList/ViewModels/RegisterViewViewModal.swift
+++ TodoList/ViewModels/RegisterViewViewModal.swift
@@ -5,6 +5,8 @@
 //  Created by Pavan Sarvaiya on 25/09/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class RegisterViewViewModal: ObservableObject {
@@ -18,6 +20,25 @@ class RegisterViewViewModal: ObservableObject {
     
     func register() {
         // Register User
+        guard validate() else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            
+            self?.insertUserRecord(id: userId)
+        }
+    }
+    
+    private func insertUserRecord(id: String) {
+        let newUser = User(id: id, name: name, email: email, joined: Date().timeIntervalSince1970)
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users").document(id)
     }
     
     private func validate() -> Bool {
@@ -33,6 +54,11 @@ class RegisterViewViewModal: ObservableObject {
         // email@foo.com
         guard email.contains("@") && email.contains(".") else {
             errorMessage = "Please enter valid email."
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            errorMessage = "Password must be greater than 6."
             return false
         }
         
